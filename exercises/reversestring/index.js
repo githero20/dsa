e00139452dafcb2d321e10a764bbb29c9f7cac29
Omogbai Atakpu <atakpuomogbai@gmail.com>
2021-08-02 04:54:08 +0100

    first exercise

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,47 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// SOLUTION 1
+// function reverse(str) {
+//   //   const arr = str.split("");
+//   //   // turn it to an Array
+//   //   arr.reverse("");
+//   //   // reverse it
+//   //   return arr.join("");
+//   //   //turn it back to a string
+
+//   //clean up the code
+//   return str.split("").reverse().join("");
+// }
+
+// SOLUTION 2
+// function reverse(str) {
+//   let reversed = "";
+//   //this creates a 'reversed' string
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//   //this iterates through each character of the array, then adds the iterated character to the empty 'reversed' array
+//   //new for loop syntax introduced with ES 2015, the syntax is perfect for +1 increments with a definite end
+
+//   return reversed;
+// }
+
+// SOLUTION 3
+function reverse(str) {
+  //   return str.split("").reduce((reversed, character) => {
+  //     return character + reversed;
+  //   }, "");
+
+  // condensed to ES6
+  debugger;
+  return str.split("").reduce((rev, char) => char + rev, "");
+  // node inspect index.js to run debugger mode
+  // press c to continue execution
+  // type repl to input code
+}
+
+reverse("peta");
 
 module.exports = reverse;
