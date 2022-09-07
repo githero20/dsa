83cb95a85a41032f5d37e0b6970caa9b20db0cea
Omogbai Atakpu <atakpuomogbai@gmail.com>
2022-09-08 00:02:20 +0100

    add more

---
+++ exercises/random/lowestCommonAncestor.js
@@ -0,0 +1,49 @@
+// Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+
+// According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+// Example 1:
+// Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+// Output: 6
+// Explanation: The LCA of nodes 2 and 8 is 6.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+
+//  https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/discuss/541362/JavaScript-Iterative-Recursive
+
+var lowestCommonAncestor = function (root, p, q) {
+  while (root) {
+    if (root.val < p.val && root.val < q.val) {
+      root = root.right;
+    } else if (root.val > p.val && root.val > q.val) {
+      root = root.left;
+    } else {
+      break;
+    }
+  }
+  return root;
+};
+
+// Recursive
+// var lowestCommonAncestor = function(root, p, q) {
+//     if (root.val < p.val && root.val < q.val) {
+//         return lowestCommonAncestor(root.right, p, q);
+//     }
+//     if (root.val > p.val && root.val > q.val) {
+//         return lowestCommonAncestor(root.left, p, q);
+//     }
+//     return root;
+// };
