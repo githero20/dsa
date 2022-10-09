518ba9cf92417169484492d5437464c0bcd1df21
Omogbai Atakpu <oa@afarapartners.com>
2022-10-09 15:47:26 +0100

    more algos

--- exercises/random/linkedListCycle.ts
+++ exercises/random/linkedListCycle.ts
@@ -27,4 +27,76 @@ function hasCycle(head: ListNode | null): boolean {
     
 };
 
-let myList = new ListNode(4)
+// let myList = new ListNode(4)
+
+// You can basically create a LinkedList with an object if you have objects nested in each other.
+// Sort of like a nested dictionary
+
+// [3,2,0,-4]
+// 3 -> 2^ -> 0 -> 4 -> 2* 
+
+// Set<value> // set js O(1)
+
+
+// head = 3; first node in linked list
+// check if exist in set 
+    // if it does -> i found a cycle 
+      // else -> add to the Set
+
+
+// {3, 2, 0,4}
+//                      *
+// 3 -> 2^ -> 0 -> 4 -> 2*
+
+// stuct Node {
+//      val : number
+//     next: null | Node
+// }
+
+// const cycleLink = {
+//   val : 2,
+//   next: {
+//     val : 0,
+//     next: {
+//       val: -4,
+//       next: null 
+//     },
+//   }
+// }
+
+// cycleLink.next.next.next = cycleLink;
+
+// const data = {
+//   val: 3,
+//   next: cycleLink
+// }
+
+
+
+// const findCycleOne = (head) => {
+//     // ecvery node encountered
+//     // O(n) -> space
+//     const cache = new Set();    
+//     cache.add(head.val);
+//     // O(n) -> time
+//     while(head.next != null){
+//         // moving to the next
+//         head = head.next
+//         if(cache.has(head.val)){
+//           return true
+//         }else{
+//           cache.add(head.val);
+//         }
+//     }
+//     return false;
+// }
+
+
+// // 2-pointer (hare & tortoise)
+
+// const findCycletwo = (head) => {
+
+// }
+
+
+// console.log(findCycle(data))
---
+++ exercises/random/majorityElement.js
@@ -0,0 +1,72 @@
+// https://leetcode.com/problems/majority-element/
+
+
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+
+// [4,5,5,5,4]
+// Map
+// {
+//   4: {
+//     2,
+//   },
+//   5: {
+//     3
+//   }
+// }
+
+// if 5.val > n/2 {
+  // return 5
+// }
+
+// function majorityElement(nums) {
+//   let charMap = {};
+//   let n = nums.length;
+//   let cnt = 0;
+//   let maxNum = null;
+  
+//   for (let i = 0; i < n; i++) {
+//     if (charMap[nums[i]]) {
+//       charMap[nums[i]] ++
+//     } else {
+//       charMap[nums[i]] = 1
+//     }
+//     if (charMap[nums[i]] > cnt) {
+//       cnt = charMap[nums[i]]
+//       maxNum = nums[i]
+//     }
+//   }
+  
+//   return maxNum;
+// };
+
+// O(n)
+// [2,2,1,1,1,2,2]
+
+// console.log(majorityElement([2,2,1,1,1,2,2]));
+
+
+// function majorityElement(nums) {
+//     let maxNum = 0;
+//   }
+  
+//   majority
+  
+//   3/ 2 ->
+  
+//   count of majority > other count
+  
+  
+  
+  
+//     4 , 3 
+//    majority => 4;
+//   count = 0;
+//                *
+//   [2,2,1,1,1,2,4]
+  
+console.log(typeof(2))
