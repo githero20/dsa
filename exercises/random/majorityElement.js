bb41934f811db7a79b1e001aebeb8bd8555a6cfe
Omogbai Atakpu <atakpuomogbai@gmail.com>
2022-10-11 04:40:22 +0100

    add more exercises

--- exercises/random/majorityElement.js
+++ exercises/random/majorityElement.js
@@ -1,6 +1,5 @@
 // https://leetcode.com/problems/majority-element/
 
-
 // Given an array nums of size n, return the majority element.
 
 // The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
@@ -20,53 +19,50 @@
 // }
 
 // if 5.val > n/2 {
-  // return 5
+// return 5
 // }
 
-// function majorityElement(nums) {
-//   let charMap = {};
-//   let n = nums.length;
-//   let cnt = 0;
-//   let maxNum = null;
-  
-//   for (let i = 0; i < n; i++) {
-//     if (charMap[nums[i]]) {
-//       charMap[nums[i]] ++
-//     } else {
-//       charMap[nums[i]] = 1
-//     }
-//     if (charMap[nums[i]] > cnt) {
-//       cnt = charMap[nums[i]]
-//       maxNum = nums[i]
-//     }
-//   }
-  
-//   return maxNum;
-// };
+function majorityElement(nums) {
+  let charMap = {};
+  let n = nums.length;
+  let cnt = 0;
+  let maxNum = null;
+
+  for (let i = 0; i < n; i++) {
+    if (charMap[nums[i]]) {
+      charMap[nums[i]]++;
+    } else {
+      charMap[nums[i]] = 1;
+    }
+
+    if (charMap[nums[i]] > cnt) {
+      cnt = charMap[nums[i]];
+      maxNum = nums[i];
+    }
+  }
+
+  return maxNum;
+}
 
 // O(n)
 // [2,2,1,1,1,2,2]
 
 // console.log(majorityElement([2,2,1,1,1,2,2]));
 
-
 // function majorityElement(nums) {
 //     let maxNum = 0;
 //   }
-  
+
 //   majority
-  
+
 //   3/ 2 ->
-  
+
 //   count of majority > other count
-  
-  
-  
-  
-//     4 , 3 
+
+//     4 , 3
 //    majority => 4;
 //   count = 0;
 //                *
 //   [2,2,1,1,1,2,4]
-  
-console.log(typeof(2))
+
+console.log(typeof 2);
--- exercises/random/reverseLinkedList.js
+++ exercises/random/reverseLinkedList.js
@@ -18,27 +18,27 @@ var reverseList = function (head) {
   return curr;
 };
 
-// Iterative Approach
-// Assume that we have linked list 1 → 2 → 3 → Ø, we would like to change it to Ø ← 1 ← 2 ← 3.
+// // Iterative Approach
+// // Assume that we have linked list 1 → 2 → 3 → Ø, we would like to change it to Ø ← 1 ← 2 ← 3.
 
-// While you are traversing the list, change the current node's next pointer to point to its previous element.
-// Since a node does not have reference to its previous node, you must store its previous element beforehand.
-// You also need another pointer to store the next node before changing the reference. Do not forget to return the new head reference at the end!
+// // While you are traversing the list, change the current node's next pointer to point to its previous element.
+// // Since a node does not have reference to its previous node, you must store its previous element beforehand.
+// // You also need another pointer to store the next node before changing the reference. Do not forget to return the new head reference at the end!
 
-var reverseList = function (head) {
-  let prev = null;
-  //   initialize it to null, like what we want i.e.  Ø ← 1 ← 2 ← 3.
-  let curr = head;
+// var reverseList = function (head) {
+//   let prev = null;
+//   //   initialize it to null, like what we want i.e.  Ø ← 1 ← 2 ← 3.
+//   let curr = head;
 
-  while (curr != null) {
-    let nextTemp = curr.next;
-    // this temporarily saves the next node
-    curr.next = prev;
-    // this changes the next nodes pointer to the previous one
-    prev = curr;
-    // this updates the previous pointer to the current value
-    curr = nextTemp;
-    // this updates the current pointer to the next one
-  }
-  return prev;
-};
+//   while (curr != null) {
+//     let nextTemp = curr.next;
+//     // this temporarily saves the next node
+//     curr.next = prev;
+//     // this changes the next nodes pointer to the previous one
+//     prev = curr;
+//     // this updates the previous pointer to the current value
+//     curr = nextTemp;
+//     // this updates the current pointer to the next one
+//   }
+//   return prev;
+// };
