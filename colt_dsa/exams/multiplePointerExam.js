40d6b4071fc7a17d3cc2559682552d5b3cb14595
Omogbai Atakpu <atakpuomogbai@gmail.com>
2024-12-01 20:56:06 +0100

    add exercises

--- colt_dsa/exams/multiplePointerExam.js
+++ colt_dsa/exams/multiplePointerExam.js
@@ -95,3 +95,33 @@ function averagePair(arr, target) {
 
 averagePair([-1, 0, 3, 4, 5, 6], 4.1); // false
 averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8); // true
+
+// Iterative
+function isSubsequence(str1, str2) {
+  // good luck. Add any arguments you deem necessary.
+
+  //   our two pointers
+  let i = 0;
+  let j = 0;
+
+  if (!str1) return true;
+
+  while (j < str2.length) {
+    // checks if the first letter exists in both
+    // if it does, then increase the count for the subset
+    if (str2[j] === str1[i]) i++;
+
+    // at this point, the subset is complete
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
+}
+
+// Recursive
+// function isSubsequence(str1, str2) {
+//   if (str1.length === 0) return true;
+//   if (str2.length === 0) return false;
+//   if (str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1));
+//   return isSubsequence(str1, str2.slice(1));
+// }
