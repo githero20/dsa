569ed2c71fa9883fa6f81a9a61cf7b22c8b46350
Omogbai Atakpu <oatakpu@boi.ng>
2025-01-02 08:44:15 +0100

    add algo

---
+++ exercises/random/arrLast.js
@@ -0,0 +1,14 @@
+// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
+
+// Input: nums = [null, {}, 3]
+// Output: 3
+// Explanation: Calling nums.last() should return the last element: 3.
+
+// Hint: Use 'this' keyword
+Array.prototype.last = function () {
+  if (this.length === 0) {
+    return -1;
+  }
+
+  return this[this.length - 1];
+};
