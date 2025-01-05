483a1ebb2f3cea74d749e4dbd1a2ef64ba425a0b
Omogbai Atakpu <atakpuomogbai@gmail.com>
2025-01-05 10:36:03 +0100

    add multiply strings question

---
+++ exercises/random/multiplyString.js
@@ -0,0 +1,15 @@
+// Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+// Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+// Example 1:
+// Input: num1 = "2", num2 = "3"
+// Output: "6"
+
+// Example 2:
+// Input: num1 = "123", num2 = "456"
+// Output: "56088"
+
+// https://leetcode.com/problems/multiply-strings/solutions/1495073/multiply-strings/
+
+// How to multiply in decimal without actually multplying
+var multiply = function (num1, num2) {};
